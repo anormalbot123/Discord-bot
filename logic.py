b89c89946a09141f96c29661cd975391f4651510
anormalbot123 <167912275+anormalbot123@users.noreply.github.com>
2025-05-22 17:45:03 -0400

    Update logic.py
    Se implemento una funcion mas para enviar nombres de anime aleatorios usando una API

--- logic.py
+++ logic.py
@@ -1,4 +1,5 @@
 import random
+import requests
 
 def gen_pass(pass_length):
     elements = "+-/*!&$#?=@<>"
@@ -8,3 +9,10 @@ def gen_pass(pass_length):
         password += random.choice(elements)
 
     return password
+
+def get_anime_name():
+    url = 'https://kitsu.io/api/edge/anime'
+    res = requests.get(url)
+    data = res.json()
+    anime = random.choice(data["data"])
+    return (anime["attributes"]["titles"]["en_jp"])
