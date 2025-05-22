b6420107466e60d900ce71c9512bbfaad0c25812
anormalbot123 <167912275+anormalbot123@users.noreply.github.com>
2025-05-22 17:42:39 -0400

    Mas commands y renombre de client.py a bot.py
    Se implementaron los comandos:
    random_anime - envia un nombre aleatorio de un anime usando una API
    meme - envia un meme aleatorio con dos memes siendo mas raros que los otros usando archivos
    Se cambio el comando help a uno personalizado, incluyendo una descripcion de cada comando

---
+++ bot.py
@@ -0,0 +1,79 @@
+from settings import *
+import logic
+import os
+import random
+
+import asyncio
+import discord
+from discord.ext import commands
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable bot y transferirle los privilegios
+bot = commands.Bot(command_prefix=setting["prefix"], intents=intents, help_command=None)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    # Says when a member joined.
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.event
+async def on_message_edit(before, after):
+    msg = f'**{before.author}** edited their message:\n{before.content} -> {after.content}'
+    await before.channel.send(msg)
+
+@bot.command()
+async def editme(ctx):
+    msg = await ctx.send('10')
+    await asyncio.sleep(3.0)
+    await msg.edit(content='40')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def password(ctx, length = 8):
+    await ctx.send(logic.gen_pass(length))
+
+@bot.command()
+async def meme(ctx):
+    ind = random.randint(1, 7)
+    img_name = os.listdir('images')
+    img_name.remove('rare')
+    rare_name = os.listdir('images/rare')
+    img = f'rare/{(random.choice(rare_name))}' if ind == 7 else random.choice(img_name)
+    with open(f'images/{img}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+    if ind == 7:
+        await ctx.send('Tienes suerte! Acaba de salir un meme de pocas probabilidades!')
+
+@bot.command()
+async def random_anime(ctx):
+    await ctx.send(logic.get_anime_name())
+
+@bot.command()
+async def help(ctx):
+    await ctx.send("""$hello - el bot saludara
+$heh # - el bot dira he # veces
+$password # - generara una password con la longitud indicada
+$editme - un mensaje para flexear
+$meme - manda un meme aleatorio
+$random_anime - manda un nombre de anime aleatorio
+nota - comandos con # significa que aceptan un numero pero no es obligatorio""")    
+
+bot.run(setting["token"])
