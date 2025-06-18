4622d9dd388c20d06a0f6c6c0568f1f14c5a1dd5
anormalbot123 <167912275+anormalbot123@users.noreply.github.com>
2025-06-18 17:30:36 -0400

    Sine_function logic
    Este acepta los parametros de amplitud, periodo, desplazamiento horizontal y vertical y devuelve un string con una funcion con esas mismas propiedades, si no tiene un parametro especificado no se va a mostrar en la funcion

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 import random
 import requests
+import math
 
 def gen_pass(pass_length):
     elements = "+-/*!&$#?=@<>"
@@ -16,3 +17,11 @@ def get_anime_name():
     data = res.json()
     anime = random.choice(data["data"])
     return (anime["attributes"]["titles"]["en_jp"])
+
+def do_sine_func(amp=1, per=2 * math.pi, x_shift=0, y_shift=0):
+    a = "" if amp == 1 else amp
+    b = "" if per == 2 * math.pi else 2 * math.pi / per
+    c = "" if x_shift == 0 else f"+ {x_shift * -1}"
+    d = "" if y_shift == 0 else f"+ {y_shift}"
+    func = f'{a}sine({b}x{c}){d}'
+    return func
